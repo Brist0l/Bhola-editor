b214376e3e9c18c55b771d3357fa71251d5a7c07
gautam <gautamthegreat21@gmail.com>
2021-06-10 14:46:06 -0700

    Update start_win.py

--- text-editor/start_win.py
+++ text-editor/start_win.py
@@ -2,6 +2,7 @@ import tkinter
 
 start = tkinter.Tk()
 tkinter.Label(start, text="Which languages do you work with?").grid()
-
-
+tkinter.Checkbutton(start, text="Python").grid()
+tkinter.Checkbutton(start, text="Java").grid()
+tkinter.Checkbutton(start, text="JavaScript").grid()
 start.mainloop()
